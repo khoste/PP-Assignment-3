312892e056ad37e0312634759da62b9dea3b155d
khoste <44264663+khoste@users.noreply.github.com>
2018-10-31 21:06:37 +0100

    Update kernel.cu

--- assignment-3/code/kernel.cu
+++ assignment-3/code/kernel.cu
@@ -18,9 +18,9 @@ __device__ inline float compute_pixel(float *trap, float omega, int x, int y, in
 __global__ void simple(float *trap, int h, int w, float omega, float epsilon, int iter, float *delta) {
 	// implement me
   
-printf("this is trap %f\n",*trap);
+printf("this is trap %f\n",trap);
 
-printf("this is delta %f\n", *delta);
+printf("this is delta %f\n", delta);
  
 }
 

28a718035710806903afbe6266a094105572045d
khoste <44264663+khoste@users.noreply.github.com>
2018-10-31 06:18:24 +0100

    Update kernel.cu

--- assignment-3/code/kernel.cu
+++ assignment-3/code/kernel.cu
@@ -17,6 +17,19 @@ __device__ inline float compute_pixel(float *trap, float omega, int x, int y, in
  */
 __global__ void simple(float *trap, int h, int w, float omega, float epsilon, int iter, float *delta) {
 	// implement me
+   	const int t_ID = threadIdx.x + blockIdx.y * blockDim.x;
+
+
+  if (threadIdx.x == 4 && blockIdx.y == 3){
+  	// printf("t_Id=",t_ID);
+  int py = threadIdx.x;
+  int px = blockIdx.y;
+  // if(blockIdx.y == 5 && threadIdx.x == 3){
+  	printf("threadIdx.x=%d, blockIdx.y=%d, blockDim.x=%d, t_ID=%d, px=%d, py=%d\n",threadIdx.x, blockIdx.y, blockDim.x, t_ID, px, py);
+  	trap1[t_ID] = compute_pixel(trap, omega, py, px, w);
+  	// printf("trap[%d]=%f\n", t_ID, trap[t_ID]);
+  	delta=fabs(trap1[t_ID]-trap[t_ID]);
+	
 }
 
 /*
